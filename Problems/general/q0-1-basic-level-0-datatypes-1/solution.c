634021080f20eb3e41d6080f1b559f6d7484a5fb
mrajesh0328 <mrajesh0328@gmail.com>
2026-09-16 16:24:23 +0530

    Solve: [Q0.1] Basic_level_0_Datatypes_1 (Medium) - C | SEED-IT

---
+++ Problems/general/q0-1-basic-level-0-datatypes-1/solution.c
@@ -0,0 +1,17 @@
+/**
+ * Problem: Q0.1 - Basic_level_0_Datatypes_1
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-16
+ * Language: c
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Let's learn 'C' together with \"SEED\" Team");
+    // your code goes here
+    return 0;
+}
